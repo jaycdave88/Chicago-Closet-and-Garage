cf3da7904acc0505e39a3069b98c85d236745d89
jaycdave88 <jaycdave@gmail.com>
2015-12-03 13:59:41 -0600

    added ajax for website - still not done - contact and services do not work and all the views are stacking on top of each other

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,29 @@
 class WelcomeController < ApplicationController
 
-  def index
-  end
+	def index
+	end
+
+	def dashboard_load
+	end
+
+	def shortcut_load
+	end
+
+	def overview_load
+	end
+
+	def event_load
+	end
+
+	def about_load
+	end
+
+	def services_load
+	end
+
+	def contact_load
+	end
+
+
 
 end
