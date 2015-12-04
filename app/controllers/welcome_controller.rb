7a9c9de5352bf2cf10570fd1a0e90ae940f61777
Jay Dave <jaycdave@gmail.com>
2015-12-03 22:19:58 -0600

    still working on getting ajax to work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,28 +1,56 @@
 class WelcomeController < ApplicationController
 
 	def index
+		# params["link_id"].match(/_link/)
+		# page = $` 
+
+		@page_info = params[:link_id]
+		this = @page_info.to_s
+		this.match(/_link/)
+		
+		# @page = "#{this}_load"
+		page_content = "#{$'}_page_content"
+		puts page_content
+
+		# puts @page
+		@page = page_content
+
+
+		respond_to do |format|
+
+			format.html
+		  format.js
+		end
+
+		# respond_to do |format|
+		# 	@page = "hello"
+		# 	format.js
+		# end
+		# puts params
+		# @page = params
+				# puts page
 	end
 
-	def dashboard_load
-	end
+	# def dashboard_load
+	# end
 
-	def shortcut_load
-	end
+	# def shortcut_load
+	# end
 
-	def overview_load
-	end
+	# def overview_load
+	# end
 
-	def event_load
-	end
+	# def event_load
+	# end
 
-	def about_load
-	end
+	# def about_load
+	# end
 
-	def services_load
-	end
+	# def services_load
+	# end
 
-	def contact_load
-	end
+	# def contact_load
+	# end
 
 
 
