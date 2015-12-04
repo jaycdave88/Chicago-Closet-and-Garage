ec318db91161b5f0fd9cf3fb87f1bff40d99d73b
Jay Dave <jaycdave@gmail.com>
2015-12-04 13:54:47 -0600

    ajax is finally working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,5 @@
 class WelcomeController < ApplicationController
 
 	def index
-		@page_info = params[:link_id]
-		this = @page_info.to_s
-		page_content = this.chomp("_link") + "_page_content"
-		# p"***************************************"
-		# 	puts page_content
-		# p"***************************************"
-		@page = page_content
-		respond_to do |format|
-			format.html
-		  format.js
-		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root :to => 'welcome#index'
 
   get '/dashboard' ,to: "welcome#dashboard_load"
     get '/shortcut' ,to: "welcome#shortcut_load"
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
           get '/about' ,to: "welcome#about_load"
             get '/services' ,to: "welcome#services_load"
               get '/contact' ,to: "welcome#contact_load"
-
 end
