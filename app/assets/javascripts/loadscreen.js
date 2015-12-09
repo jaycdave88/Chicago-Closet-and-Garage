d102aac34c3e65db5e1101e8fc3b8189451a2841
jaycdave88 <jaycdave@gmail.com>
2015-12-09 10:09:18 -0600

    removed some css from my timmer menthod for screen saver

--- app/assets/javascripts/loadscreen.js
+++ app/assets/javascripts/loadscreen.js
@@ -2,14 +2,5 @@ $(document).ready(function() {
   setTimeout(function(){
     $('body').addClass('loaded');
     $('h1').css('color','#222222');
-    $(".load").css("position: ' '");
   }, 9350);  
 });
-
-
-// window.onload = function(){
-//  var load_screen = document.getElementById('loader-wrapper');
-//  console.log("fuk");
-//  document.body.removeChild(load_screen)
-// } 
-// ***not working: need to get the self timer working
