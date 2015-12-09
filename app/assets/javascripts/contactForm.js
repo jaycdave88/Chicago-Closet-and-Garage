0c9ac4422a578b06fc6773b9e4ce1d1a6646a9cc
Jay Dave <jaycdave@gmail.com>
2015-12-08 19:45:53 -0600

    working version of partial contact page

---
+++ app/assets/javascripts/contactForm.js
@@ -0,0 +1,21 @@
+var testing = $(".submit_contactMe");
+testing.click(function(){
+  // if() execute code here to find the names of the field are not empty
+  if ($("#name").val()=='' || $("#email").val()=='' || $("#subject").val()=='' || $("#message").val()=='') {
+    return;
+  };
+  var contactForm = $("#contactForm");
+  $.ajax({
+    url: contactForm.attr("action"),
+    type: "POST",
+    data: contactForm.serializeArray(),
+    success:function(result, status){
+      $("#name").val('');
+      $("#email").val('');
+      $("#subject").val('');
+      $("#message").val('');
+      
+    }
+  })
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   root :to => 'welcome#index'
 
   get '/dashboard' ,to: "welcome#dashboard_load"
-    get '/shortcut' ,to: "welcome#shortcut_load"
-      get '/overview' ,to: "welcome#overview_load"
-        get '/event' ,to: "welcome#event_load"
-          get '/about' ,to: "welcome#about_load"
-            get '/services' ,to: "welcome#services_load"
-              get '/contact' ,to: "welcome#contact_load"
-                get '/home', to: "welcome#home_load"
+  get '/shortcut' ,to: "welcome#shortcut_load"
+  get '/overview' ,to: "welcome#overview_load"
+  get '/event' ,to: "welcome#event_load"
+  get '/about' ,to: "welcome#about_load"
+  get '/services' ,to: "welcome#services_load"
+  get '/contact_page' ,to: "welcome#contact_load"
+  get '/home', to: "welcome#home_load"
+
+  resources :contact, except: [:new, :create, :edit, :update, :destroy, :show]
+
+  namespace :api do
+    namespace :v1 do
+      resources :contact
+    end
+  end
 end
