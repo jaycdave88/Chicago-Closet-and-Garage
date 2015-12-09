c581f7f6f65b20d1c2df307509effcf73a50b3c3
jaycdave88 <jaycdave@gmail.com>
2015-12-09 11:09:43 -0600

    added logic to the intro screen and renamed the side bar and views

--- app/assets/javascripts/loadscreen.js
+++ app/assets/javascripts/loadscreen.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   setTimeout(function(){
     $('body').addClass('loaded');
     $('h1').css('color','#222222');
-  }, 9350);  
-});
+    $('.s3gon').css('opacity: " " ')
+  }, 9250);  
+})
