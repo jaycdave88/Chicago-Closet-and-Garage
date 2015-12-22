4e8800a8fb7d3128abce8f3eb55e0d0b00c0d258
Jay Dave <jaycdave@gmail.com>
2015-12-22 14:03:37 -0600

    fucking finally finished wiht the damn contact page.

--- app/controllers/api/v1/contact_controller.rb
+++ app/controllers/api/v1/contact_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::ContactController < ActionController::Base
 
 	def create
-    
 		begin
-      @hasFinished = false
-
       if params['name'] == "" || params['email'] == "" || params['subject'] == ""
         puts "The required information was not provided"
       else
-        @hasFinished = true
         ContactMailer.send_message(params).deliver
       end 
 		end
